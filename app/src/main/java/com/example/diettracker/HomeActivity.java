ff77a94b549fdacc1b0eacc63517b587a6fb3ff5
princeton woods <vpprinceton@aol.com>
2026-07-26 18:59:57 -0500

    Connected Welcome page to HomeActivity.java

--- app/src/main/java/com/example/diettracker/HomeActivity.java
+++ app/src/main/java/com/example/diettracker/HomeActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,6 +21,7 @@ public class HomeActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 openViewFoodItem();
             }
         });
@@ -30,11 +31,4 @@ public class HomeActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ViewFoodItem.class);
         startActivity(intent);
     }
-
-    EditText simpleEditText = (EditText) findViewById(R.id.userInput);
-
-    String strValue = simpleEditText.getText().toString();
-
-    //convert it to integer type for calculations
-    int number = Integer.parseInt(strValue);
 }
