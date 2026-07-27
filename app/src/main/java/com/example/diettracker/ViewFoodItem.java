71ab5cb2afe7ab46bd3b6021ddf58d3da9ed487b
thuymmai <thuymmai11@gmail.com>
2026-07-27 02:05:48 -0500

    Added tool bar in activity_view_food_item.xml

--- app/src/main/java/com/example/diettracker/ViewFoodItem.java
+++ app/src/main/java/com/example/diettracker/ViewFoodItem.java
@@ -3,6 +3,7 @@ package com.example.diettracker;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.SystemBarStyle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,14 +11,16 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ViewFoodItem extends AppCompatActivity {
 
+    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        EdgeToEdge.enable(this, SystemBarStyle.dark(1));
         setContentView(R.layout.activity_view_food_item);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(systemBars.left, 0, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
